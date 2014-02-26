669ad03961ac8b1a0d50eadc8efea779b8a0f559
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2014-02-26 21:55:52 +0000

    Improved URL and Member Handling in ViewCount();

--- function.viewcount.php
+++ function.viewcount.php
@@ -7,15 +7,13 @@
 function ViewCount() {
 
 	// Set some Globals
-	global $Canonical, $Member_Auth, $Member_Name, $Member_Mail, $Member_Admin, $Time, $Request, $MySQL_Connection, $Sitewide_Root, $User_IP, $User_Cookie;
+	global $Canonical, $Member_Auth, $Member_ID, $Member_Admin, $Time, $Place, $MySQL_Connection, $Sitewide_Root, $User_IP, $User_Cookie;
 
 	// Build Query
-	// TODO Expand Request to Include Query Strings
-	$Query = 'INSERT INTO `Views` (`Request`, `Canonical`, `IP`, `Cookie`, `Auth`, `Mail`, `Admin`, `Time`) VALUES (\''.$Request['scheme'].'://'.$Request['host'].$Request['path'].'\', \''.$Canonical.'\', \''.$User_IP.'\', \''.$User_Cookie.'\', \''.$Member_Auth.'\', \''.$Member_Mail.'\', \''.$Member_Admin.'\', \''.$Time.'\')';
+	$Query = 'INSERT INTO `Views` (`Request`, `Canonical`, `IP`, `Cookie`, `Auth`, `Member_ID`, `Admin`, `Time`) VALUES (\''.$Place['scheme'].'://'.$Place['host'].$_SERVER['REQUEST_URI'].'\', \''.$Canonical.'\', \''.$User_IP.'\', \''.$User_Cookie.'\', \''.$Member_Auth.'\', \''.$Member_ID.'\', \''.$Member_Admin.'\', \''.$Time.'\')';
 	// echo $Query;
 
 	$View = mysqli_query($MySQL_Connection, $Query, MYSQLI_STORE_RESULT);
 	if (!$View) exit('Invalid Query (View): '.mysqli_error($MySQL_Connection));
-
 
 }
