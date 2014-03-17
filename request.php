9ef33428d9378cfef499299f5a2259dc7fc29513
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2014-03-17 21:18:30 +0000

    Update Include-Path Setting

--- request.php
+++ request.php
@@ -4,9 +4,15 @@
 	date_default_timezone_set('UTC');
 	// Note: GMT is deprecated. Use UTC instead.
 
-	// ini_set('include_path', dirname(__FILE__));
-	// set_include_path(dirname(__FILE__));
-	set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__).'/broadcast');
+	// Set the inclusion
+	if (!defined('PATH_SEPARATOR')) {
+		if (strpos($_ENV['OS'], 'Win') !== false) define('PATH_SEPARATOR', ';');
+		else define('PATH_SEPARATOR', ':');
+	}
+	$Include_Path = get_include_path().PATH_SEPARATOR.dirname(__FILE__).PATH_SEPARATOR.dirname(__FILE__).DIRECTORY_SEPARATOR.'broadcast'.PATH_SEPARATOR.dirname(__FILE__).DIRECTORY_SEPARATOR.'public_html';
+	if (version_compare(PHP_VERSION, '4.3.0', '>=')) set_include_path($Include_Path);
+	else ini_set('include_path', $Include_Path);
+
 
 	// Include the Configuration
 	// This is not require because it might fail if not yet installed.
