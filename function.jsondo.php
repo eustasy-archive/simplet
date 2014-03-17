6a0d2b224cd5774be70c79576fba1260445ebaa0
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2014-03-17 21:18:17 +0000

    Shorten JSONDo Function

--- function.jsondo.php
+++ function.jsondo.php
@@ -6,10 +6,7 @@
 
 function JSONDo($Array) {
 
-	if (version_compare(PHP_VERSION, '5.4.0', '>=')) {
-		echo json_encode($Array, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-	} else {
-		echo json_encode($Array);
-	}
+	if (version_compare(PHP_VERSION, '5.4.0', '>=')) echo json_encode($Array, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+	else echo json_encode($Array);
 
 }
