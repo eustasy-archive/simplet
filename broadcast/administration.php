1b87e80bb72df1f3d660c3580370c2d4738601f9
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2014-03-27 19:29:53 +0000

    Add Administration File

---
+++ broadcast/administration.php
@@ -0,0 +1,49 @@
+<?php
+
+	$Title_HTML = 'Administration';
+	$Title_Plain = 'Administration';
+
+	$Description_HTML = 'Administration';
+	$Description_Plain = 'Administration';
+
+	$Keywords = 'administration password email';
+
+	$Featured_Image = '';
+
+	$Canonical = 'administration';
+
+	$Post_Type = 'Page';
+	$Post_Category = '';
+
+	require_once __DIR__.'/../request.php';
+
+	$Administration = $Canonical; // Canonical may change later, Account won't
+	$Header = '../header.php';
+	$Footer = '../footer.php';
+	$Lib_Browning_Config = __DIR__.'/../libs/Browning_Config.php';
+	$Lib_Browning_Send = __DIR__.'/../libs/Browning_Send.php';
+
+if ($Request['path'] === $Place['path'].$Canonical) {
+
+	if (!$Member_Auth) { // Login Redirect
+		header('Location: '.$Place['path'].'account', TRUE, 302);
+		die();
+
+	} else if (isset($_GET['key'])) {
+
+		$Title_HTML = 'Administration';
+		$Title_Plain = 'Administration';
+
+		$Description_HTML = 'Administration';
+		$Description_Plain = 'Administration';
+
+		$Keywords = 'administration password email';
+
+		$Canonical = 'administration';
+
+		require $Header;
+
+		require $Footer;
+
+	}
+}
