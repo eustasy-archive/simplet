858725c93f05c35b94df6db5f4a22f7cb790b075
Lewis Goddard <lewisgoddard@eustasy.org>
2014-08-02 12:52:17 +0100

    Improved MySQL Failure Error

--- onces/connect.php
+++ onces/connect.php
@@ -7,7 +7,7 @@ if(
 	!empty($Database_Name)
 	) {
 	$MySQL_Connection = mysqli_connect($Database_Host, $Database_User, $Database_Pass, $Database_Name);
-	if (!$MySQL_Connection) $MySQL_Connection_Error = mysqli_connect_error($MySQL_Connection);
+	if (!$MySQL_Connection) $MySQL_Connection_Error = 'Connection Failed. Check your configuration is correct. <!-- Simplet MySQL Error: '.mysqli_connect_error($MySQL_Connection).' -->';
 	else $MySQL_Connection_Error = false;
 } else {
 	$MySQL_Connection = false;
