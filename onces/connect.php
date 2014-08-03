4049d9ac41cb0c2f5c30382d4bc786657a20d86a
Lewis Goddard <lewisgoddard@eustasy.org>
2014-08-03 11:42:28 +0100

    Update connect.php

--- onces/connect.php
+++ onces/connect.php
@@ -31,8 +31,27 @@ CREATE TABLE IF NOT EXISTS `Members` (
 	`Modified` int(11) NOT NULL,
 	UNIQUE KEY `ID` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;';
+	// TODO Add Key to Mail
 			$Create_Table_Members = mysqli_query($MySQL_Connection, $Create_Table_Members, MYSQLI_STORE_RESULT);
 			if ($Sitewide_Debug && !$Create_Table_Members) echo 'Invalid Query ($Create_Table_Members): '.mysqli_error($MySQL_Connection);
+		}
+		
+		if ($Sitewide_Database_AutoInstall && !Database_Table_Exists('Sessions')) {
+			$Create_Table_Sessions = '
+CREATE TABLE IF NOT EXISTS `Sessions` (
+	`ID` int(255) NOT NULL AUTO_INCREMENT,
+	`Member_ID` varchar(12) NOT NULL,
+	`Mail` varchar(255) NOT NULL,
+	`IP` varchar(255) NOT NULL,
+	`Cookie` varchar(64) NOT NULL,
+	`Active` int(1) NOT NULL,
+	`Created` int(11) NOT NULL,
+	`Modified` int(11) NOT NULL,
+	UNIQUE KEY `ID` (`ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;';
+	// TODO Add Key to Member_ID and nMail and Cookie and Active
+			$Create_Table_Sessions = mysqli_query($MySQL_Connection, $Create_Table_Sessions, MYSQLI_STORE_RESULT);
+			if ($Sitewide_Debug && !$Create_Table_Sessions) echo 'Invalid Query ($Create_Table_Sessions): '.mysqli_error($MySQL_Connection);
 		}
 		
 		// TODO Add Creation for Other Tables
