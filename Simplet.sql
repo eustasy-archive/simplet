a34674af3544c33300ed731c2230909ed20fc9bf
Lewis Goddard <lewisgoddard@eustasy.org>
2014-08-04 21:57:05 +0100

    Update Database with new keys

--- Simplet.sql
+++ Simplet.sql
@@ -21,16 +21,17 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `Members` (
-  `ID` varchar(12) NOT NULL,
-  `Mail` varchar(250) NOT NULL,
-  `Name` varchar(250) NOT NULL,
-  `Admin` int(1) NOT NULL,
-  `Status` varchar(100) NOT NULL,
-  `Pass` varchar(1000) NOT NULL,
-  `Salt` varchar(64) NOT NULL,
-  `Created` int(11) NOT NULL,
-  `Modified` int(11) NOT NULL,
-  UNIQUE KEY `ID` (`ID`)
+	`ID` varchar(12) NOT NULL,
+	`Mail` varchar(250) NOT NULL,
+	`Name` varchar(250) NOT NULL,
+	`Admin` int(1) NOT NULL,
+	`Status` varchar(100) NOT NULL,
+	`Pass` varchar(1000) NOT NULL,
+	`Salt` varchar(64) NOT NULL,
+	`Created` int(11) NOT NULL,
+	`Modified` int(11) NOT NULL,
+	UNIQUE KEY `ID` (`ID`),
+	PRIMARY KEY `Mail` (`Mail`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -40,16 +41,20 @@ CREATE TABLE IF NOT EXISTS `Members` (
 --
 
 CREATE TABLE IF NOT EXISTS `Sessions` (
-  `ID` int(255) NOT NULL AUTO_INCREMENT,
-  `Member_ID` varchar(12) NOT NULL,
-  `Mail` varchar(255) NOT NULL,
-  `IP` varchar(255) NOT NULL,
-  `Cookie` varchar(64) NOT NULL,
-  `Active` int(1) NOT NULL,
-  `Created` int(11) NOT NULL,
-  `Modified` int(11) NOT NULL,
-  UNIQUE KEY `ID` (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+	`ID` int(255) NOT NULL AUTO_INCREMENT,
+	`Member_ID` varchar(12) NOT NULL,
+	`Mail` varchar(255) NOT NULL,
+	`IP` varchar(255) NOT NULL,
+	`Cookie` varchar(64) NOT NULL,
+	`Active` int(1) NOT NULL,
+	`Created` int(11) NOT NULL,
+	`Modified` int(11) NOT NULL,
+	UNIQUE KEY `ID` (`ID`),
+	PRIMARY KEY `Member_ID` (`Member_ID`),
+	INDEX `Mail` (`Mail`),
+	INDEX `Cookie` (`Cookie`),
+	INDEX `Active` (`Active`)
+) ENGINE=InnoDB	DEFAULT CHARSET=latin1;';
 
 -- --------------------------------------------------------
 
@@ -58,13 +63,13 @@ CREATE TABLE IF NOT EXISTS `Sessions` (
 --
 
 CREATE TABLE IF NOT EXISTS `Failures` (
-  `ID` int(255) NOT NULL AUTO_INCREMENT,
-  `Member_ID` varchar(12) NOT NULL,
-  `Mail` varchar(255) NOT NULL,
-  `IP` varchar(255) NOT NULL,
-  `Created` int(11) NOT NULL,
-  UNIQUE KEY `ID` (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+	`ID` int(255) NOT NULL AUTO_INCREMENT,
+	`Member_ID` varchar(12) NOT NULL,
+	`Mail` varchar(255) NOT NULL,
+	`IP` varchar(255) NOT NULL,
+	`Created` int(11) NOT NULL,
+	UNIQUE KEY `ID` (`ID`)
+) ENGINE=InnoDB	DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -73,14 +78,14 @@ CREATE TABLE IF NOT EXISTS `Failures` (
 --
 
 CREATE TABLE IF NOT EXISTS `Runonce` (
-  `Member_ID` varchar(12) NOT NULL,
-  `Key` varchar(64) NOT NULL,
-  `Status` varchar(12) NOT NULL,
-  `IP` varchar(64) NOT NULL,
-  `Created` int(11) NOT NULL,
-  `Modified` int(11) NOT NULL,
-  `Notes` mediumtext NOT NULL,
-  UNIQUE KEY `Key` (`Key`)
+	`Member_ID` varchar(12) NOT NULL,
+	`Key` varchar(64) NOT NULL,
+	`Status` varchar(12) NOT NULL,
+	`IP` varchar(64) NOT NULL,
+	`Created` int(11) NOT NULL,
+	`Modified` int(11) NOT NULL,
+	`Notes` mediumtext NOT NULL,
+	UNIQUE KEY `Key` (`Key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -90,15 +95,15 @@ CREATE TABLE IF NOT EXISTS `Runonce` (
 --
 
 CREATE TABLE IF NOT EXISTS `Categories` (
-  `Member_ID` varchar(12) NOT NULL,
-  `Status` varchar(12) NOT NULL,
-  `Title` varchar(255) NOT NULL,
-  `Slug` varchar(255) NOT NULL,
-  `Description` varchar(255) NOT NULL,
-  `Topics` int(10) NOT NULL,
-  `Created` int(11) NOT NULL,
-  `Modified` int(11) NOT NULL,
-  PRIMARY KEY (`Slug`)
+	`Member_ID` varchar(12) NOT NULL,
+	`Status` varchar(12) NOT NULL,
+	`Title` varchar(255) NOT NULL,
+	`Slug` varchar(255) NOT NULL,
+	`Description` varchar(255) NOT NULL,
+	`Topics` int(10) NOT NULL,
+	`Created` int(11) NOT NULL,
+	`Modified` int(11) NOT NULL,
+	PRIMARY KEY (`Slug`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -108,15 +113,15 @@ CREATE TABLE IF NOT EXISTS `Categories` (
 --
 
 CREATE TABLE IF NOT EXISTS `Topics` (
-  `Member_ID` varchar(12) NOT NULL,
-  `Status` varchar(12) NOT NULL,
-  `Category` varchar(255) NOT NULL,
-  `Slug` varchar(500) NOT NULL,
-  `Title` varchar(500) NOT NULL,
-  `Responses` int(10) NOT NULL,
-  `Created` int(11) NOT NULL,
-  `Modified` int(11) NOT NULL,
-  PRIMARY KEY (`Slug`)
+	`Member_ID` varchar(12) NOT NULL,
+	`Status` varchar(12) NOT NULL,
+	`Category` varchar(255) NOT NULL,
+	`Slug` varchar(500) NOT NULL,
+	`Title` varchar(500) NOT NULL,
+	`Responses` int(10) NOT NULL,
+	`Created` int(11) NOT NULL,
+	`Modified` int(11) NOT NULL,
+	PRIMARY KEY (`Slug`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -126,18 +131,18 @@ CREATE TABLE IF NOT EXISTS `Topics` (
 --
 
 CREATE TABLE IF NOT EXISTS `Responses` (
-  `ID` int(255) NOT NULL AUTO_INCREMENT,
-  `Member_ID` varchar(12) NOT NULL,
-  `Canonical` varchar(500) NOT NULL,
-  `Type` varchar(12) NOT NULL,
-  `Status` varchar(12) NOT NULL,
-  `Helpfulness` int(11) NOT NULL DEFAULT '0',
-  `Rating` int(12) NOT NULL,
-  `Post` mediumtext NOT NULL,
-  `Created` int(11) NOT NULL,
-  `Modified` int(11) NOT NULL,
-  UNIQUE KEY `ID` (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+	`ID` int(255) NOT NULL AUTO_INCREMENT,
+	`Member_ID` varchar(12) NOT NULL,
+	`Canonical` varchar(500) NOT NULL,
+	`Type` varchar(12) NOT NULL,
+	`Status` varchar(12) NOT NULL,
+	`Helpfulness` int(11) NOT NULL DEFAULT '0',
+	`Rating` int(12) NOT NULL,
+	`Post` mediumtext NOT NULL,
+	`Created` int(11) NOT NULL,
+	`Modified` int(11) NOT NULL,
+	UNIQUE KEY `ID` (`ID`)
+) ENGINE=InnoDB	DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -146,15 +151,15 @@ CREATE TABLE IF NOT EXISTS `Responses` (
 --
 
 CREATE TABLE IF NOT EXISTS `Helpfulness` (
-  `ID` int(255) NOT NULL AUTO_INCREMENT,
-  `Response_Canonical` varchar(500) NOT NULL,
-  `Response_ID` int(255) NOT NULL,
-  `Member_ID` varchar(12) NOT NULL,
-  `Helpfulness` varchar(4) NOT NULL,
-  `Created` int(12) NOT NULL,
-  `Modified` int(12) NOT NULL,
-  UNIQUE KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+	`ID` int(255) NOT NULL AUTO_INCREMENT,
+	`Response_Canonical` varchar(500) NOT NULL,
+	`Response_ID` int(255) NOT NULL,
+	`Member_ID` varchar(12) NOT NULL,
+	`Helpfulness` varchar(4) NOT NULL,
+	`Created` int(12) NOT NULL,
+	`Modified` int(12) NOT NULL,
+	UNIQUE KEY (`ID`)
+) ENGINE=InnoDB	DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -163,15 +168,15 @@ CREATE TABLE IF NOT EXISTS `Helpfulness` (
 --
 
 CREATE TABLE IF NOT EXISTS `Views` (
-  `ID` int(255) NOT NULL AUTO_INCREMENT,
-  `Request` varchar(2500) NOT NULL,
-  `Canonical` varchar(2500) NOT NULL,
-  `Post_Type` varchar(255) NOT NULL,
-  `IP` varchar(255) NOT NULL,
-  `Cookie` varchar(64) NOT NULL,
-  `Auth` varchar(5) NOT NULL,
-  `Member_ID` varchar(12) NOT NULL,
-  `Admin` varchar(5) NOT NULL,
-  `Time` int(12) NOT NULL,
-  PRIMARY KEY (`ID`)
+	`ID` int(255) NOT NULL AUTO_INCREMENT,
+	`Request` varchar(2500) NOT NULL,
+	`Canonical` varchar(2500) NOT NULL,
+	`Post_Type` varchar(255) NOT NULL,
+	`IP` varchar(255) NOT NULL,
+	`Cookie` varchar(64) NOT NULL,
+	`Auth` varchar(5) NOT NULL,
+	`Member_ID` varchar(12) NOT NULL,
+	`Admin` varchar(5) NOT NULL,
+	`Time` int(12) NOT NULL,
+	PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
