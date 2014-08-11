8f986a6356c209a444a6e7c1edaf76b7d67c12d8
Lewis Goddard <lewisgoddard@eustasy.org>
2014-08-11 11:50:08 +0100

    Fix Table_Exists for Prefix support

--- functions/database.table.exists.php
+++ functions/database.table.exists.php
@@ -6,22 +6,24 @@
 //
 // Database_Table_Exists('Topics');
 
-function Database_Table_Exists($Table_Name) {
+function Database_Table_Exists($Table_Name. $AddPrefix = true) {
 	
 	// Set some Globals
 	global $Database;
 	
- 	$Database_Table_Exists_Query = 'SELECT * FROM `information_schema`.`TABLES` WHERE `TABLE_SCHEMA`=\''.$Database['Name'].'\' AND `TABLE_NAME`=\''.$Table_Name.'\'';
- 	
+	$Database_Table_Exists_Query = 'SELECT * FROM `information_schema`.`TABLES` WHERE `TABLE_SCHEMA`=\''.$Database['Name'].'\' AND `TABLE_NAME`=\'';
+	if ( $AddPrefix ) $Database_Table_Exists_Query .= $Database['Prefix'];
+	$Database_Table_Exists_Query .= $Table_Name.'\'';
+	
 	$Database_Table_Exists_Query = mysqli_query($Database['Connection'], $Database_Table_Exists_Query, MYSQLI_STORE_RESULT);
 	
-	if (!$Database_Table_Exists_Query) {
+	if ( !$Database_Table_Exists_Query ) {
 		echo 'Invalid Query (Key_Check): '.mysqli_error($Database['Connection']);
 		return false;
 	}
 	
 	$Database_Table_Exists_Query_Count = mysqli_num_rows($Database_Table_Exists_Query);
-	if ($Database_Table_Exists_Query_Count > 0) return true;
+	if ( $Database_Table_Exists_Query_Count > 0 ) return true;
 	else return false;
 	
 }
