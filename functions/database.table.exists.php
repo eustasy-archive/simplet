b9aadce6d71dd9785971f6cfb53ab8f75d8473bc
Lewis Goddard <lewisgoddard@eustasy.org>
2014-08-16 14:43:15 +0100

    Working through functions part 3

--- functions/database.table.exists.php
+++ functions/database.table.exists.php
@@ -1,28 +1,38 @@
 <?php
 
-// ### Database Table Exists Function ###
+//// Database Table Exists Function
 //
 // Checks whether or not a table exists.
 //
 // Database_Table_Exists('Topics');
 
 function Database_Table_Exists($Table_Name, $AddPrefix = true) {
-
+	
 	global $Database;
 	
+	// Assemble the Query
 	$Database_Table_Exists_Query = 'SELECT * FROM `information_schema`.`TABLES` WHERE `TABLE_SCHEMA`=\''.$Database['Name'].'\' AND `TABLE_NAME`=\'';
 	if ( $AddPrefix ) $Database_Table_Exists_Query .= $Database['Prefix'];
 	$Database_Table_Exists_Query .= $Table_Name.'\'';
 	
+	// Execute the Query
+	// SPECIAL CASE: This query doesn't need to be inside a table exists check because it is querying a table from outside Simplet.
 	$Database_Table_Exists_Query = mysqli_query($Database['Connection'], $Database_Table_Exists_Query, MYSQLI_STORE_RESULT);
 	
+	// IFNOTQUERY: The query failed.
 	if ( !$Database_Table_Exists_Query ) {
 		echo 'Invalid Query (Key_Check): '.mysqli_error($Database['Connection']);
 		return false;
-	}
 	
-	$Database_Table_Exists_Query_Count = mysqli_num_rows($Database_Table_Exists_Query);
-	if ( $Database_Table_Exists_Query_Count > 0 ) return true;
-	else return false;
+	// The Query was executed successfully.
+	} else {
+		// Fetch the count.
+		$Database_Table_Exists_Query_Count = mysqli_num_rows($Database_Table_Exists_Query);
+		// If the count is positive it exists.
+		if ( $Database_Table_Exists_Query_Count > 0 ) return true;
+		// If the count is zero it does not.
+		else return false;
 	
+	} // IFNOTQUERY
+
 }
