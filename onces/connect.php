21cbd6ab7aab0d42e41a57cffae0caf43642851f
lewisgoddard <lewisgoddard@eustasy.org>
2014-09-16 23:56:49 +0100

    Seperate config.database and add example

--- onces/connect.php
+++ onces/connect.php
@@ -2,34 +2,7 @@
 
 $Database = array();
 
-////	START Database Configuration
-
-// Database Host
-// 'localhost' or an IP Address
-$Database['Host'] = 'localhost';
-
-// Database User
-// You probably shouldn't use 'Simplet' here.
-$Database['User'] = 'Simplet';
-
-// Database Pass
-$Database['Pass'] = 'Password1';
-
-// Database Name
-// You might want to change the Database Name too.
-$Database['Name'] = 'Simplet';
-
-// Database Name
-// You might want to change the Database Name too.
-$Database['Prefix'] = 'Simplet_';
-
-// Fatal on Database Error
-$Database['FatalOnError'] = false;
-
-// AutoInstall Database Tables
-$Database['AutoInstall'] = true;
-
-////	END Database Configuration
+include_once '../config.database.php';
 
 if(
 	!empty($Database['Host']) &&
