81a0145c12180c97332520cc212e9e362342d162
Lewis Goddard <lewisgoddard@eustasy.org>
2014-09-30 23:58:13 +0100

    Add Results as modal

--- functions/unit-tests/auto-test.php
+++ functions/unit-tests/auto-test.php
@@ -7,6 +7,7 @@
 		<style>
 		body {
 			max-width: 800px;
+			height: 100%;
 			margin: 0 auto;
 		}
 		code {
@@ -49,13 +50,38 @@
 		.display-none {
 			display: none;
 		}
-		/*
-			.top {
-				font-weight: bold;
-			}
-		*/
+		/* External */
+		#lean_overlay {
+			background: #fff;
+			cursor: pointer;
+			display: none;
+			height:100%;
+			left: 0px;
+			position: fixed;
+			top: 0px;
+			width:100%;
+			z-index:100;
+		}
+		.expand {
+			cursor: pointer;
+		}
+		.expandable {
+			padding: 20px;
+			border: 1px solid #eee;
+			border-radius: 5px;
+			background: #fafafa;
+			max-width: 1000px;
+			max-height: 60%;
+			overflow-y: scroll;
+		}
+		pre {outline: 1px solid #ccc; padding: 5px; margin: 5px; }
+		.string { color: green; }
+		.number { color: darkorange; }
+		.boolean { color: blue; }
+		.null { color: magenta; }
+		.key { color: red; }
 		</style>
-		<script src="https://cdn.jsdelivr.net/g/jquery,tablesorter"></script>
+		<script src="https://cdn.jsdelivr.net/g/jquery,tablesorter,jquery.leanmodal"></script>
 	</head>
 	<body>
 		<header>
@@ -69,11 +95,33 @@
 				<tr>
 					<th>Name</th>
 					<th>Status</th>
+					<th></th>
 				</tr>
 			</thead>
 			<tbody>
 			</tbody>
 		</table>
+		<script>
+			$(function() {
+				// Based on code from http://jsfiddle.net/KJQ9K/554/
+				function syntaxHighlight(json) {
+					json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+					return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+						var cls = 'color-pumpkin'; // Number
+						if (/^"/.test(match)) {
+							if (/:$/.test(match)) {
+								cls = 'color-midnight-blue'; // Key
+							} else {
+								cls = 'color-nephritis'; // String
+							}
+						} else if (/true|false/.test(match)) {
+							cls = 'color-belize-hole'; // Boolean
+						} else if (/null/.test(match)) {
+							cls = 'color-alizarin'; // null
+						}
+						return '<span class="' + cls + '">' + match + '</span>';
+					});
+				}
 <?php
 	// Get a list of the tests.
 	$Tests = glob('*.php');
@@ -85,11 +133,7 @@
 			unset($Tests[$Key]);
 		}
 	}
-?>
-		<script>
-			$(function() {
-				<?php
-	$Load = 'var load = [';
+	$Load = '				var load = [';
 	// For each test
 	foreach ($Tests as $Key => $Value) {
 		$Load .= '
@@ -108,6 +152,7 @@
 				$('table').tablesorter({ sortForce: sorting });
 				function autoTest() {
 					console.log('Loading ' + load[i]);
+					var modalAppend = toAppend = false;
 					$.getJSON(
 						load[i],
 						function( data ) {
@@ -118,17 +163,21 @@
 								toAppend = '\
 			<tr class="top">\
 				<td class="align-left">' + data.Name + '</td>\
-				<td class="align-center background-nephritis color-white">Success</td>\
-			</tr>';
+				<td class="align-center background-nephritis color-white">Success</td>';
 								if ( data.Result ) {
-									var results = JSON.stringify(data.Result);
+									var results = syntaxHighlight(JSON.stringify(data.Result, undefined, '	'));
 									toAppend += '\
-			<tr class="background-clouds">\
-				<td colspan="2">\
+				<td class="align-center background-belize-hole color-white expand" href="#modal_' + i + '">\
+					+\
+				</td>';
+									modalAppend = '\
+				<div id="modal_' + i + '" class="expandable display-none">\
 					<code>' + results + '</code>\
-				</td>\
+				</div>';
+								} else toAppend += '\
+				<td></td>';
+								toAppend += '\
 			</tr>';
-								}
 								// Update Counter
 								success += 1;
 								if ( success > 1 ) $('.js-target-count-success').text(success + ' Successes');
@@ -142,23 +191,28 @@
 								else toAppend += '\
 				<td class="align-left">' + load[i] + '</td>';
 								toAppend += '\
-				<td class="align-center background-pomegranate color-white pad-10">Failure</td>\
-			</tr>';
+				<td class="align-center background-pomegranate color-white pad-10">Failure</td>';
 								if ( data.Errors ) {
-									var errors = JSON.stringify(data.Errors);
+									var errors = syntaxHighlight(JSON.stringify(data.Errors));
 									toAppend += '\
-			<tr class="background-clouds">\
-				<td colspan="2" class="pad-10">\
+				<td class="align-center background-belize-hole color-white expand" href="#modal_' + i + '">\
+					+\
+				</td>';
+									modalAppend = '\
+				<div id="modal_' + i + '" class="expandable display-none">\
 					<code>' + errors + '</code>\
-				</td>\
+				</div>';
+								} else toAppend += '\
+				<td></td>';
+								toAppend += '\
 			</tr>';
-								}
 								// Update Counter
 								failures += 1;
 								if ( failures > 1 ) $('.js-target-count-failures').text(failures + ' Failures');
 								else $('.js-target-count-failures').text('1 failures').removeClass('display-none');
 							}
 							$('tbody').append(toAppend);
+							if ( modalAppend ) $('body').append(modalAppend);
 							$('table').trigger('update');
 							$('table').trigger('sorton',[sorting]);
 							i++;
@@ -180,6 +234,12 @@
 						$('table').trigger('sorton',[sorting]);
 						i++;
 						if ( i < length ) autoTest();
+					});
+					$('.expand').leanModal();
+					$('.expand').click(function() {
+						console.log('Expand clicked.');
+						var modal_id = $(this).attr('href');
+						console.log(modal_id);
 					});
 				}
 				autoTest();
