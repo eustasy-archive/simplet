341c4acc9b1dd978fa8e3ccd651a3b54214de08a
Lewis Goddard <lewisgoddard@eustasy.org>
2014-10-02 23:50:39 +0100

    Remove var_dump

--- broadcast/account.php
+++ broadcast/account.php
@@ -732,7 +732,6 @@ if ($Request['path'] === $Place['path'].$Canonical) {
 				require $Header;
 
 				$Key = Runonce_Create($Time+(60*3), 1, 'Account Deletion');
-				var_dump($Key);
 				?>
 
 				<h2>Are you sure you want to delete your account?</h2>
