ec361fd8fc0c241f32fee108acc28d2306044da0
Lewis Goddard <lewisgoddard@eustasy.org>
2014-10-09 22:37:31 +0100

    Update connection.php

--- broadcast/security/connection.php
+++ broadcast/security/connection.php
@@ -30,6 +30,7 @@ if ($Request['path'] === $Place['path'].$Canonical) {
 	<p>We get an A+ rating from Qualys SSL Lab for our implementation of Perfect Forward Secrecy, HTTP Strict Transport Security with a long duration, Session resumption, and Strict Transport Security. We've made efforts to remove outdated and vulnerable cipher suites, and prefer TLS over SSL. These result in a PCI compliant and FIPS-ready server.</p>
 
 	<h3><a href="http://spdycheck.org/#security.simplet.eustasy.org">SPDY</a></h3>
+	<p>Thanks in part to Nginx (and the community that funded the feature), we support SPDY as a connection method, decreasing the handshake time required while maintaining security.</p>
 
 	<h3><a href="http://ipv6-test.com/validate.php?url=security.simplet.eustasy.org&scheme=https">IPv6</a></h3>
 	<p class="textcenter">We support IPv6. Not much else to say about this.</p>
