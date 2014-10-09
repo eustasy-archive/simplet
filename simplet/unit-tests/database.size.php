4a27bbfa264d36163f414957c29f76a60584a718
Lewis Goddard <lewisgoddard@eustasy.org>
2014-10-09 22:10:39 +0100

    Add Database_Size

---
+++ simplet/unit-tests/database.size.php
@@ -0,0 +1,17 @@
+<?php
+
+include_once __DIR__.'/../auto-test_initialize.php';
+include_once __DIR__.'/../functions/database.size.php';
+$Return['Name'] = 'Database Size';
+$Return['Status'] = 'Failure';
+
+$Database_Size = Database_Size();
+
+if ( $Database_Size ) {
+	$Return['Status'] = 'Success';
+	$Return['Result'] = $Database_Size;
+} else {
+	$Return['Errors'] = 'Function returned false.';
+}
+
+echo API_Output($Return);
