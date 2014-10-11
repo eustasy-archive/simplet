712c9ed12b83c7fff55e8f407478177eb5eadf80
Lewis Goddard <lewisgoddard@eustasy.org>
2014-10-11 23:32:32 +0100

    Update Known/Fixed Issues

--- broadcast/security/levels.php
+++ broadcast/security/levels.php
@@ -74,13 +74,15 @@ if ($Request['path'] === $Place['path'].$Canonical) {
 
 	<h3>Level 4 - Known Issues</h3>
 	<h6><em>No bounties will be awarded for Level 4 issues.</em></h6>
-	<p><strong>Cross-Site Request Forgery:</strong> Many forms can be submitted from foreign pages. Referral tracking is unreliable, so all forms should also carry a single-use authentication token, implemented using the RunOnce functions. <strong>Known to affect all versions.</strong></p>
-	<p><strong>Insecure content loading:</strong> The Responses system, among others, makes use of the service "<a href="https://en.gravatar.com/">Gravatar</a>". Some content is loaded over a fixed http conection, without proper encryption. <strong>Known to affect all versions.</strong></p>
-	<p><strong>XML Conversion oddities:</strong>Because numeric keys are not valid in XML, the API converts the items to <code>"item###"</code>, which could inadvertently cause issues with Member_IDs and other automatically generated values that have a small chance of being entirely numeric. <code>(10/36)^12 = 2.11042533e-7</code> <strong>Known to affect 4.1+</strong></p>
+	<p><strong><a href="https://github.com/eustasy/simplet/issues/26">Cross-Site Request Forgery</a>:</strong> Many forms can be submitted from foreign pages. Referral tracking is unreliable, so all forms should also carry a single-use authentication token, implemented using the RunOnce functions. <strong>Known to affect all versions.</strong></p>
+	<p><strong>XML Conversion oddities:</strong>Because numeric keys are not valid in XML, the API converts the items to <code>"item###"</code>, which could inadvertently cause issues with Member_IDs and other automatically generated values that have a small chance of being entirely numeric. <code>(10/36)^12 = 2.11042533e-7</code> <strong>Known to affect 4.1+, not listed for "repair".</strong></p>
+	<p><strong><a href="https://github.com/eustasy/simplet/issues/39">SQL Injection: Inputs with a <code>\</code> are not properly escaped</a>:</strong> This issue can cause a variety of posting issues, and could be the key to far greater SQL-based attacks. <strong>Known to affect all versions.</strong></p>
 
 	<h3>Level 5 - Fixed Issues</h3>
 	<h6><em>Fixed in development or even a previous release. No bounties.</em></h6>
-	<p><strong>External Redirection:</strong>Members can be redirected to external site after prompting for a session. Should be optional, and non-default, labelled as a potential security risk. <strong>Partial Fix in version 4. Disabled entirely, possibly an option in a later release.</strong></p>
+	<p><strong>External Redirection:</strong> Members can be redirected to external site after prompting for a session. Should be optional, and non-default, labelled as a potential security risk. <strong>Partial Fix in version 4. Disabled entirely, possibly an option in a later release.</strong></p>
+	<p><strong>XSS: <a href="https://github.com/eustasy/simplet/issues/35">Insecure content loading</a>:</strong> The Responses system, among others, makes use of the service "Gravatar". Some content is loaded over a fixed http connection, without proper encryption. <strong>Fixed in <a href="https://github.com/eustasy/simplet/commit/b5c96ecbe94e83cb9beaef1f495463ce73dfff83">revision b5c96ec</a>, due for release as part of version 5.</strong></p>
+	<p><strong>XSS: <a href="https://github.com/eustasy/simplet/issues/37">Forum Topic titles don't suitable escape html in the category indexes</a>:</strong> On output Topic Titles are un-encoded and can be used to execute arbitary code in a users browser when looking at category indexes. <strong>Fixed in <a href="https://github.com/eustasy/simplet/compare/1dd17593bd141a5ffd5655aff00a4ea69d01d28e...695f16eed4efb0abb7e239a3f86431a1b68637ae">1dd1759...695f16e</a>, due for release as part of version 5.</strong></p>
 
 	<?php
 	Responses();
