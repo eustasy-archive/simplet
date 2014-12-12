a0afc6a81880e6eb9e140cd7dbc3338d05519b2f
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2014-12-12 21:07:51 +0000

    Added Strict-Transport-Security fixes #72

--- simplet/onces/headers.php
+++ simplet/onces/headers.php
@@ -1,3 +1,16 @@
 <?php
 
+// X-Frame-Options
+// Stops pages being displayed in iFrames.
+// Default: DENY
+// Options:
+// - SAMEORIGIN
+// - ALLOW-FROM $var
 header('X-Frame-Options: DENY');
+
+// Strict-Transport-Security
+// Disallows connections over insecure channels.
+// Defaults to on for https, off for http.
+if ( $Request['Secure'] ) {
+	header('Strict-Transport-Security: max-age=31536000');
+}
