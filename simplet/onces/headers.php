797ac1ec8a09d801817c4e9c1b02129393f24c30
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2014-12-18 14:35:47 +0000

    Employ CORS and CSP (Closes #67)

--- simplet/onces/headers.php
+++ simplet/onces/headers.php
@@ -7,6 +7,18 @@ if ( $Request['Secure'] ) {
 	header('Strict-Transport-Security: max-age=31536000');
 }
 
+// Access-Control-Allow-Origin
+// CORS: Cross-Origin Resource Sharing
+header('Access-Control-Allow-Origin: origin');
+
+// Content-Security-Policy
+// X-Content-Security-Policy
+// X-WebKit-CSP
+$CSP = 'default-src \'self\'; script-src \'self\' \'unsafe-inline\' cdn.jsdelivr.net 1.2.3.8 *.google-analytics.com; style-src \'self\' \'unsafe-inline\' cdn.jsdelivr.net; font-src \'self\' \'unsafe-inline\' cdn.jsdelivr.net; img-src *; media-src * mediastream:; frame-src *; object-src *; child-src *; frame-ancestors \'none\'; form-action \'self\'; connect-src \'self\'; report-uri \'//'.$Sitewide_Root.'/api?csp-report\';';
+header('X-WebKit-CSP: '.$CSP);
+header('X-Content-Security-Policy: '.$CSP);
+header('Content-Security-Policy: '.$CSP);
+
 // X-Content-Type-Options
 // Stop content loading as a different MIME Type.
 // Default: NOSNIFF
